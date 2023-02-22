e1d71df363d2013702a06f396bc2674799db00d5
mmshihov <m.m.shihov@gmail.com>
2023-02-22 11:30:44 +0300

    Ames: isPointInPattern_XY test

--- eimes-room/software/py/ames-img.py
+++ eimes-room/software/py/ames-img.py
@@ -368,7 +368,7 @@ def mirrorOy(pattern: Pattern):
     return patternMulMatrix3D(pattern, m)
 
 # точки узора должны образовывать выпуклый многоугольник
-def isPointInPattern_xOy(point: list[float], pattern: Pattern):
+def isPointInPattern_XY(point: list[float], pattern: Pattern):
     pointsCount = len(pattern.points)
     if pointsCount < 3:
         return False
@@ -420,6 +420,34 @@ def makePicture(basePattern, basePlane,  amesPattern, amesPlane):
     mAmes, mAmes_rev = matrixFor_XY(amesPlane)
     amesPatternXY = patternMulMatrix3D(amesPattern, mAmes)
     amesX, amesY, amesLenX, amesLenY = patternArea_XY(amesPatternXY)
-    
+
+
+pattern = Pattern("foo", [
+    [ 0,  5,  0],
+    [ 4,  2,  0],
+    [ 2, -3,  0],
+    [-4,  1,  0],
+])
+
+def isFalse(falseCondition, msg):
+    if not falseCondition:
+        print("OK")
+        return
+    print("FAIL: ", msg)
+
+def isTrue(trueCondition, msg):
+    if trueCondition:
+        print("OK")
+        return
+    print("FAIL: ", msg)
+
+isTrue( isPointInPattern_XY([ 1,  2,  0], pattern), "0")
+isTrue( isPointInPattern_XY([ 0,  0,  0], pattern), "1")
+isTrue( isPointInPattern_XY([-2,  1,  0], pattern), "2")
+isFalse(isPointInPattern_XY([-3,  3,  0], pattern), "3")
+isFalse(isPointInPattern_XY([ 2,  5,  0], pattern), "4")
+isFalse(isPointInPattern_XY([ 4,  0,  0], pattern), "5")
+isTrue (isPointInPattern_XY([-1,  3,  0], pattern), "6")
+isFalse(isPointInPattern_XY([-2, -2,  0], pattern), "7")
 
 print("Done. Use image files in the 'output' directory")
